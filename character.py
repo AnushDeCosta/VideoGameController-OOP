85da167d7377ba23d8f0ef40fb577d76ba7479f0
Anush De Costa <anush.de.costa@gmail.com>
2025-05-10 16:46:17 +1000

    Updated character.py
    # Initial project setup
    1. Implemented dodge() method to display action message
    2. Added docstring to dodge()
    3. Verified jump() and dodge() methods through test code

--- character.py
+++ character.py
@@ -8,7 +8,6 @@ class Character:
         """
         Initializes the character with a name, a default arrow count of 6,
         and sets jumping status to False.
-
         :param name: str - The name of the character
         """
         self.__name = name
@@ -18,7 +17,6 @@ class Character:
     def get_name(self):
         """
         Returns the name of the character.
-
         :return: str
         """
         return self.__name
@@ -26,7 +24,6 @@ class Character:
     def get_arrows(self):
         """
         Returns the current number of arrows the character has.
-
         :return: int
         """
         return self.__arrows
@@ -34,7 +31,6 @@ class Character:
     def get_is_jumping(self):
         """
         Returns a string representation of the character's jumping state.
-
         :return: str - "Jumping" or "Not Jumping"
         """
         if self.__is_jumping == False:
@@ -44,17 +40,19 @@ class Character:
 
     def jump(self):
         """
-
-        :return:
+        Makes the character jump by updating their jumping status and
+        displaying a message.
+        :return: None, Prints a message
         """
-        pass
+        self.__is_jumping = True
+        print(f"{self.__name} jumps in the air")
 
     def dodge(self):
         """
-
-        :return:
+        Displays a message indicating the character has dodged an attack.
+        :return: None
         """
-        pass
+        print(f"{self.__name} swiftly evades the enemy.")
 
     def aim_bow(self):
         """
@@ -86,4 +84,5 @@ class Character:
 
 # Test code
 character = Character("Thomas")
-print(f"Your character's name is {character.get_name()}, You have {character.get_arrows()} arrows left and is {character.get_is_jumping()}")
+character.jump()
+character.dodge()
